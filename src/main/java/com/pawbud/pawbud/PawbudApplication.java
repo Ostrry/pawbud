114dafebec5838dc8258babfa82b327aae7fe2f7
Stanislaw <wloga.marcin@gmail.com>
2019-12-07 21:58:45 +0100

    FirstCommit

---
+++ src/main/java/com/pawbud/pawbud/PawbudApplication.java
@@ -0,0 +1,13 @@
+package com.pawbud.pawbud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawbudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PawbudApplication.class, args);
+	}
+
+}
